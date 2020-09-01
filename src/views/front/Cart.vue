44dd86bcb50a847212b367ef1f870bfb71f8e013
billy <yaj55ushydna@gmail.com>
2020-09-02 00:02:27 +0800

    0902ver

--- src/views/front/Cart.vue
+++ src/views/front/Cart.vue
@@ -1,149 +1,128 @@
 <template>
-  <div class="">
+  <div class="page">
     <loading :active.sync="isLoading"></loading>
-    <div class="container">
-      <navbar></navbar>
-      <div class="page mt-6">
-        <ul class="shop-step">
-          <li class="shop-step__list">
-            <div class="shop-step__num">1</div>
-            <h4 class="shop-step__text">
-              購物車訂單<i class="fas fa-shopping-cart"></i>
-            </h4>
-          </li>
-          <li class="shop-step__list shop-step__list--arrow">
-            <i class="fas fa-angle-right"></i>
-          </li>
-          <li class="shop-step__list">
-            <div class="shop-step__num">2</div>
-            <h4 class="shop-step__text">
-              收件人資料<i class="fas fa-edit"></i>
-            </h4>
-          </li>
-          <li class="shop-step__list shop-step__list--arrow">
-            <i class="fas fa-angle-right"></i>
-          </li>
-          <li class="shop-step__list">
-            <div class="shop-step__num">3</div>
-            <h4 class="shop-step__text">
-              購物完成<i class="fas fa-check-circle"></i>
-            </h4>
-          </li>
-        </ul>
-        <div class="row">
-          <div class="col-md-8">
-            <table class="table table-cart">
-              <!-- <thead>
-                <tr>
-                  <th scope="col" class="border-0 pl-0">品名</th>
-                  <th scope="col" class="border-0">數量</th>
-                  <th scope="col" class="border-0">單價</th>
-                  <th scope="col" class="border-0">刪除</th>
-                </tr>
-              </thead> -->
-              <tbody class="table-cart__tbody">
-                <tr class="table-cart__tr" v-for="item in carts" :key="item.product.id">
-                  <td class="border-0 table-cart__td">
-                    <img :src="item.product.imageUrl[0]" alt="" class="table-cart__pic"
-                    style="width: 100px; height: 64px; object-fit: cover;">
-                  </td>
-                  <td class="border-0 table-cart__td" style="max-width: 160px;">
-                    <p class="mb-0 font-weight-bold ml-3 d-inline-block">
-                      {{ item.product.title }}
-                    </p>
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <button class="btn btn-outline-dark border-0 py-2"
-                        type="button" id="button-addon1">
-                          <i class="fas fa-minus"></i>
-                        </button>
-                      </div>
-                      <input type="text" class="form-control border-0
-                      text-center my-auto shadow-none"
-                      placeholder=""
-                      aria-label="Example text with button addon"
-                      aria-describedby="button-addon1" value="1">
-                      <div class="input-group-append">
-                        <button class="btn btn-outline-dark border-0 py-2"
-                        type="button" id="button-addon2">
-                          <i class="fas fa-plus"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="border-0 table-cart__td">
-                    <p class="mb-0 ml-auto">
-                      NT$ {{ item.product.price }}
-                    </p>
-                  </td>
-                  <td class="border-0 table-cart__td">
-                    <i class="fas fa-trash-alt"></i>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+    <navbar></navbar>
+    <section class="section">
+      <div class="cart-page">
+        <div class="container">
+          <!-- 流程部分 -->
+          <ul class="shop-step">
+            <li class="shop-step__list">
+              <div class="shop-step__num">1</div>
+              <h4 class="shop-step__text">
+                購物車訂單<i class="fas fa-shopping-cart"></i>
+              </h4>
+            </li>
+            <li class="shop-step__list shop-step__list--arrow">
+              <i class="fas fa-angle-right"></i>
+            </li>
+            <li class="shop-step__list">
+              <div class="shop-step__num">2</div>
+              <h4 class="shop-step__text">
+                收件人資料<i class="fas fa-edit"></i>
+              </h4>
+            </li>
+            <li class="shop-step__list shop-step__list--arrow">
+              <i class="fas fa-angle-right"></i>
+            </li>
+            <li class="shop-step__list">
+              <div class="shop-step__num">3</div>
+              <h4 class="shop-step__text">
+                購物完成<i class="fas fa-check-circle"></i>
+              </h4>
+            </li>
+          </ul>
+          <!-- 流程部分 END -->
 
+          <div class="section-title" style="text-align: left;
+    border-bottom: solid 1px;
+    border-top: solid 1px;
+    padding: 11px 0;">
+            <h2 style="    ">購物車</h2>
+          <router-link to="/products" class="btn btn-primary btn-block mt-4 rounded-pill">
+                    繼續購物
+                  </router-link>
           </div>
-          <div class="col-md-4">
-            <div class="border p-4 mb-4" style="background-color: #eee">
-              <h4 class="font-weight-bold mb-4">訂單詳細</h4>
-              <table class="table text-muted border-bottom">
-                <tbody>
-                  <tr>
-                    <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">小計</th>
-                    <td class="text-right border-0 px-0 pt-4">NT$24,000</td>
-                  </tr>
-                  <!-- <tr>
-                    <th scope="row" class="border-0 px-0 pt-0 pb-4 font-weight-normal">Payment</th>
-                    <td class="text-right border-0 px-0 pt-0 pb-4">ApplePay</td>
-                  </tr> -->
-                </tbody>
-              </table>
-              <div class="d-flex justify-content-between mt-4">
-                <p class="mb-0 h4 font-weight-bold">總計</p>
-                <p class="mb-0 h4 font-weight-bold">NT$24,000</p>
+
+          <!-- 購物車內容 -->
+          <ul class="cart-info">
+            <li class="cart-info__list" v-for="item in carts" :key="item.product.id + 1">
+              <div class="cart-info__pic">
+                <img :src="item.product.imageUrl[0]" alt="">
               </div>
-              <router-link to="/checkout" class="btn btn-primary btn-block mt-4">
-                到收件人
-              </router-link>
-            </div>
-            <div class="input-group w-50">
-              <input type="text"
-              class="form-control rounded-0 border-bottom
-              border-top-0 border-left-0 border-right-0 shadow-none"
-              placeholder="折價券"
-              aria-label="Recipient's username"
-              aria-describedby="button-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-outline-dark border-bottom
-                border-top-0 border-left-0 border-right-0 rounded-0"
-                type="button" id="button-addon2"><i class="fas fa-paper-plane"></i></button>
+              <div class="cart-info__con">
+                <h4 class="cart-info__title">{{ item.product.title }}</h4>
+
+                <div class="cart-info__count">
+                  <button
+                    type="button"
+                    class="btn-cart"
+                    :disabled="item.quantity === 1"
+                    @click="item.quantity --;
+                      editCartItemNum(item.product.id, item.quantity)">
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    class="cart-info__input"
+                    v-model="item.quantity"
+                    min="1">
+                  <button
+                    type="button"
+                    class="btn-cart"
+                    @click="item.quantity ++;
+                      editCartItemNum(item.product.id, item.quantity)">
+                    +
+                  </button>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <div class="">
-          <h3 class="font-weight-bold">可能有興趣的產品</h3>
-          <div class="swiper-container mt-4 mb-5">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <div class="card border-0 mb-4 position-relative position-relative">
-                  <img src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" class="card-img-top rounded-0" alt="...">
-                  <a href="#" class="text-dark">
-                  </a>
-                  <div class="card-body p-0">
-                    <h4 class="mb-0 mt-3"><a href="#">Lorem ipsum</a></h4>
-                    <p class="card-text mb-0">
-                      NT$1,080 <span class="text-muted "><del>NT$1,200</del></span></p>
-                    <p class="text-muted mt-3"></p>
-                  </div>
+              <div class="cart-info__price">
+                ${{ item.product.price | toThousands }}
+              </div>
+              <div class="cart-info__close" @click="deleteCartItem(item.product.id)">
+                <i class="far fa-trash-alt"></i>
+              </div>
+            </li>
+          </ul>
+          <!-- 購物車內容 END -->
+
+          <div class="row">
+            <div class="col-md-8">
+              <div class="input-group w-50">
+                <input type="text"
+                class="form-control rounded-0 border-bottom
+                border-top-0 border-left-0 border-right-0 shadow-none"
+                placeholder="折價券">
+                <div class="input-group-append">
+                  <button class="btn btn-outline-dark border-bottom
+                  border-top-0 border-left-0 border-right-0 rounded-0"
+                  type="button" id="button-addon2"><i class="fas fa-paper-plane"></i></button>
                 </div>
               </div>
             </div>
+            <div class="col-md-4">
+              <div class="mb-4">
+                <div class="d-flex justify-content-between mt-4">
+                  <p class="mb-0 font-weight-bold">小計</p>
+                  <p class="mb-0 font-weight-bold">{{ cartTotal | toThousands }}</p>
+                </div>
+                <div class="d-flex justify-content-between mt-4">
+                  <p class="mb-0 h4 font-weight-bold">總計</p>
+                  <p class="mb-0 h4 font-weight-bold">{{ cartTotal | toThousands }}</p>
+                </div>
+              </div>
+            </div>
+            <router-link to="/products" class="btn btn-primary btn-block mt-4 rounded-pill">
+              繼續購物
+            </router-link>
+            <router-link to="/checkout" class="btn btn-primary btn-block mt-4 rounded-pill">
+              到收件人
+            </router-link>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
     <pagebottom></pagebottom>
   </div>
 </template>
@@ -161,6 +140,7 @@ export default {
   data() {
     return {
       carts: [],
+      cartTotal: 0,
       isLoading: false,
     };
   },
@@ -175,6 +155,41 @@ export default {
         .then((res) => {
           // console.log(res);
           this.carts = res.data.data;
+          this.updateTotal();
+          this.isLoading = false;
+        });
+    },
+    updateTotal() { // 計算總價
+      this.cartTotal = 0; // 歸零，不然計算會有累加狀況。
+      this.carts.forEach((item) => {
+        this.cartTotal += item.product.price * item.quantity;
+      });
+    },
+    deleteCartItem(id) { // 刪除購物車某筆資料
+      this.isLoading = true;
+      const url = `${process.env.VUE_APP_APIPATH}/${process.env.VUE_APP_UUID}/ec/shopping/${id}`;
+      this.axios.delete(url).then((res) => {
+        console.log(res);
+        this.getCart(); // 要重新運算
+        this.isLoading = false;
+      }).catch((error) => {
+        console.log(error);
+        this.isLoading = false;
+      });
+    },
+    editCartItemNum(id, quantity) {
+      this.isLoading = true;
+      const url = `${process.env.VUE_APP_APIPATH}/${process.env.VUE_APP_UUID}/ec/shopping`;
+      const cart = {
+        product: id,
+        quantity,
+      };
+      this.axios.patch(url, cart)
+        .then(() => {
+          this.$bus.$emit('notice-user', '課程數量改變成功');
+          this.isLoading = false;
+          this.getCart();
+        }).catch(() => {
           this.isLoading = false;
         });
     },
--- src/views/front/Checkout.vue
+++ src/views/front/Checkout.vue
@@ -1,133 +1,131 @@
 <template>
-  <div class="">
-     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-10">
-          <navbar></navbar>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-10">
-          <h3 class="font-weight-bold mb-4 pt-3">收件人資訊</h3>
-        </div>
-      </div>
-      <div class="row flex-row-reverse justify-content-center pb-5">
-        <div class="col-md-4">
-          <div class="border p-4 mb-4">
-            <div class="d-flex">
-              <img src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80" alt="" class="mr-2" style="width: 48px; height: 48px; object-fit: cover">
-              <div class="w-100">
-                <div class="d-flex justify-content-between">
-                  <p class="mb-0 font-weight-bold">Lorem ipsum</p>
-                  <p class="mb-0">NT$12,000</p>
+  <div class="page">
+    <navbar></navbar>
+    <section class="section">
+      <div class="cart-page">
+        <div class="container">
+          <div class="row justify-content-center pb-5">
+            <div class="col-md-12">
+              <div class="border p-4 mb-4">
+                <div class="d-flex">
+                  <img src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80" alt="" class="mr-2" style="width: 48px; height: 48px; object-fit: cover">
+                  <div class="w-100">
+                    <div class="d-flex justify-content-between">
+                      <p class="mb-0 font-weight-bold">Lorem ipsum</p>
+                      <p class="mb-0">NT$12,000</p>
+                    </div>
+                    <p class="mb-0 font-weight-bold">x1</p>
+                  </div>
                 </div>
-                <p class="mb-0 font-weight-bold">x1</p>
+                <div class="d-flex mt-2">
+                  <img src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80" alt="" class="mr-2" style="width: 48px; height: 48px; object-fit: cover">
+                  <div class="w-100">
+                    <div class="d-flex justify-content-between">
+                      <p class="mb-0 font-weight-bold">Lorem ipsum</p>
+                      <p class="mb-0">NT$12,000</p>
+                    </div>
+                    <p class="mb-0 font-weight-bold">x1</p>
+                  </div>
+                </div>
+                <table class="table mt-4 border-top border-bottom text-muted">
+                  <tbody>
+                    <tr>
+                      <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">小計</th>
+                      <td class="text-right border-0 px-0 pt-4">NT$24,000</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div class="d-flex justify-content-between mt-4">
+                  <p class="mb-0 h4 font-weight-bold">總計</p>
+                  <p class="mb-0 h4 font-weight-bold">NT$24,000</p>
+                </div>
               </div>
             </div>
-            <div class="d-flex mt-2">
-              <img src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80" alt="" class="mr-2" style="width: 48px; height: 48px; object-fit: cover">
-              <div class="w-100">
-                <div class="d-flex justify-content-between">
-                  <p class="mb-0 font-weight-bold">Lorem ipsum</p>
-                  <p class="mb-0">NT$12,000</p>
-                </div>
-                <p class="mb-0 font-weight-bold">x1</p>
-              </div>
-            </div>
-            <table class="table mt-4 border-top border-bottom text-muted">
-              <tbody>
-                <tr>
-                  <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">小計</th>
-                  <td class="text-right border-0 px-0 pt-4">NT$24,000</td>
-                </tr>
-              </tbody>
-            </table>
-            <div class="d-flex justify-content-between mt-4">
-              <p class="mb-0 h4 font-weight-bold">總計</p>
-              <p class="mb-0 h4 font-weight-bold">NT$24,000</p>
+            <div class="col-md-12">
+              <validation-observer v-slot="{ invalid }">
+                <form @submit.prevent="clickMe()">
+                  <!-- 收件人姓名 -->
+                  <div class="form-group">
+                    <validation-provider rules="required" v-slot="{ errors, classes }">
+                      <label for="username">收件人姓名</label>
+                      <input id="username" type="text" name="收件人姓名"
+                      v-model="username" class="form-control" :class="classes">
+                      <span class="invalid-feedback">{{ errors[0] }}</span>
+                    </validation-provider>
+                  </div>
+
+                  <!-- Email  -->
+                  <div class="form-group">
+                    <validation-provider rules="required|email" v-slot="{ errors, classes }">
+                      <label for="email">Email</label>
+                      <input id="email" type="email" name="信箱" v-model="email"
+                      class="form-control" :class="classes">
+                      <span class="invalid-feedback">{{ errors[0] }}</span>
+                    </validation-provider>
+                  </div>
+
+                  <!-- 電話 -->
+                  <div class="form-group">
+                    <validation-provider rules="required|min:8" v-slot="{ errors, classes }">
+                      <label for="tel">電話</label>
+                      <input id="tel" type="tel" name="電話" v-model="tel"
+                      class="form-control" :class="classes">
+                      <span class="invalid-feedback">{{ errors[0] }}</span>
+                    </validation-provider>
+                  </div>
+
+                  <!-- 地址 -->
+                  <div class="form-group">
+                    <validation-provider rules="required" v-slot="{ errors, classes }">
+                      <label for="addr">地址</label>
+                      <input id="addr" type="text" name="地址" v-model="addr"
+                      class="form-control" :class="classes">
+                      <span class="invalid-feedback">{{ errors[0] }}</span>
+                    </validation-provider>
+                  </div>
+
+                  <!-- 購買方式 -->
+                  <div class="form-group">
+                    <label for="pay-method">購買方式</label>
+                    <select name="付款方式" id="pay-method" class="form-control"
+                    required="required" v-model="payMethod">
+                      <option value="" disabled="disabled" selected>請選擇付款方式</option>
+                      <option value="WebATM">WebATM</option>
+                      <option value="ATM">ATM</option>
+                      <option value="Barcode">Barcode</option>
+                      <option value="Credit">Credit</option>
+                      <option value="ApplePay">ApplePay</option>
+                      <option value="GooglePay">GooglePay</option>
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="message">留言</label>
+                    <textarea name="message" id="" cols="30" rows="3"
+                    class="form-control" v-model="message"></textarea>
+                  </div>
+
+                  <button type="submit" class="btn btn-primary" :disabled="invalid">按鈕</button>
+                </form>
+              </validation-observer>
             </div>
           </div>
         </div>
-        <div class="col-md-6">
-          <validation-observer v-slot="{ invalid }">
-            <form @submit.prevent="clickMe()">
-              <!-- 收件人姓名 -->
-              <div class="form-group">
-                <validation-provider rules="required" v-slot="{ errors, classes }">
-                  <label for="username">收件人姓名</label>
-                  <input id="username" type="text" name="收件人姓名"
-                  v-model="username" class="form-control" :class="classes">
-                  <span class="invalid-feedback">{{ errors[0] }}</span>
-                </validation-provider>
-              </div>
-
-              <!-- Email  -->
-              <div class="form-group">
-                <validation-provider rules="required|email" v-slot="{ errors, classes }">
-                  <label for="email">Email</label>
-                  <input id="email" type="email" name="信箱" v-model="email"
-                  class="form-control" :class="classes">
-                  <span class="invalid-feedback">{{ errors[0] }}</span>
-                </validation-provider>
-              </div>
-
-              <!-- 電話 -->
-              <div class="form-group">
-                <validation-provider rules="required|min:8" v-slot="{ errors, classes }">
-                  <label for="tel">電話</label>
-                  <input id="tel" type="tel" name="電話" v-model="tel"
-                  class="form-control" :class="classes">
-                  <span class="invalid-feedback">{{ errors[0] }}</span>
-                </validation-provider>
-              </div>
-
-              <!-- 地址 -->
-              <div class="form-group">
-                <validation-provider rules="required" v-slot="{ errors, classes }">
-                  <label for="addr">地址</label>
-                  <input id="addr" type="text" name="地址" v-model="addr"
-                  class="form-control" :class="classes">
-                  <span class="invalid-feedback">{{ errors[0] }}</span>
-                </validation-provider>
-              </div>
-
-              <!-- 購買方式 -->
-              <div class="form-group">
-                <label for="pay-method">購買方式</label>
-                <select name="付款方式" id="pay-method" class="form-control"
-                required="required" v-model="payMethod">
-                  <option value="" disabled="disabled" selected>請選擇付款方式</option>
-                  <option value="WebATM">WebATM</option>
-                  <option value="ATM">ATM</option>
-                  <option value="Barcode">Barcode</option>
-                  <option value="Credit">Credit</option>
-                  <option value="ApplePay">ApplePay</option>
-                  <option value="GooglePay">GooglePay</option>
-                </select>
-              </div>
-
-              <div class="form-group">
-                <label for="message">留言</label>
-                <textarea name="message" id="" cols="30" rows="3"
-                class="form-control" v-model="message"></textarea>
-              </div>
-
-              <button type="submit" class="btn btn-primary" :disabled="invalid">按鈕</button>
-            </form>
-          </validation-observer>
-        </div>
       </div>
-    </div>
+    </section>
+    <pagebottom></pagebottom>
   </div>
 </template>
 
 <script>
 
 import navbar from '@/components/Navbar.vue';
+import pagebottom from '@/components/Footer.vue';
 
 export default {
   components: {
     navbar,
+    pagebottom,
   },
   data() {
     return {

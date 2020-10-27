f03081ef073b074492a3beb50d3108636e158b53
billy <yaj55ushydna@gmail.com>
2020-10-27 11:47:55 +0800

    v4

--- src/filters/tothousands.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-Vue.filter('toThousands', (para = 0) => {
-  const parts = para.toString().split('.');
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  return parts.join('.');
-});
